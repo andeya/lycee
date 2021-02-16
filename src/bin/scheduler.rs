a9ac3960fda379face0587ab7d0f333a2c630d4d
henrylee2cn <henrylee2cn@gmail.com>
2021-02-16 14:11:49 +0800

    Update scheduler.rs
    Change-Id: I4de02ac3d712adf7e3274ae73a0af4a22bef55a8

--- src/bin/scheduler.rs
+++ src/bin/scheduler.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("I'm scheduler!");
+use tonic::{Request, Response, Status, transport::Server};
+
+use lycee::proto::helloworld::{HelloReply, HelloRequest};
+use lycee::proto::helloworld::greeter_server::{Greeter, GreeterServer};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50052".parse()?;
+    let greeter = MyGreeter::default();
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
